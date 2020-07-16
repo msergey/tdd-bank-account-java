7b03b67d8533ce59b026a9358c034881914a7306
Sergey Sorokin <sergey_sorokin@outlook.com>
2020-07-16 09:57:30 -0400

    Removed Java 11 specific code

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,20 +8,20 @@ public class AccountTest {
 
     @Test
     public void newBalancesShouldHaveZeroBalance() {
-        var account = new Account();
+        Account account = new Account();
         assertThat(account.balance()).isEqualTo(0);
     }
 
     @Test
     public void depositAmountToSetTheBalance() {
-        var account = new Account();
+        Account account = new Account();
         account.deposit(10);
         assertThat(account.balance()).isEqualTo(10);
     }
 
     @Test
     public void depositMultipleAmountsToIncreaseTheBalance() {
-        var account = new Account();
+        Account account = new Account();
         account.deposit(10);
         account.deposit(20);
         assertThat(account.balance()).isEqualTo(30);
